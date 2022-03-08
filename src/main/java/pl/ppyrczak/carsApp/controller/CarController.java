34aa2a21fa520364d808076f099be55e32c48fc4
Piotr Pyrczak <ppyrczak5@gmail.com>
2022-03-08 21:26:28 +0100

    .

--- src/main/java/pl/ppyrczak/carsApp/controller/CarController.java
+++ src/main/java/pl/ppyrczak/carsApp/controller/CarController.java
@@ -1,20 +1,25 @@
 package pl.ppyrczak.carsApp.controller;
 
 
+import org.apache.tomcat.jni.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import pl.ppyrczak.carsApp.model.Car;
+import pl.ppyrczak.carsApp.model.CarRepository;
 import pl.ppyrczak.carsApp.model.SqlCarRepository;
 
+import javax.validation.Valid;
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
 
 @RepositoryRestController
 public class CarController {
-    private final SqlCarRepository carRepository;
+    private final CarRepository carRepository;
     public static final Logger logger = LoggerFactory.getLogger(CarController.class);
 
     public CarController(SqlCarRepository carRepository) {
@@ -27,11 +32,36 @@ public class CarController {
         return ResponseEntity.ok(carRepository.findAll());
     }
 
-    @GetMapping("users")
+    @GetMapping("cars")
     ResponseEntity<List<Car>> getAllCars() {
         return new ResponseEntity<>(carRepository.findAll(), HttpStatus.OK);
     }
 
-    @GetMapping("users/{id}")
-    ResponseEntity<Car>
+    @GetMapping("/cars/{id}")
+    ResponseEntity<Car> getCarById(@PathVariable int id) {
+        return carRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping("/cars")
+    ResponseEntity<Car> createCar(@RequestBody @Valid Car carToCreate) {
+        Car car = carRepository.save(carToCreate);
+        return ResponseEntity.created(URI.create("/" + car.getId())).body(car);
+    }
+
+    @PutMapping("/tasks/{id}")
+    ResponseEntity<?> updateCar(@PathVariable int id, @RequestBody Car carToUpdate) {
+        if(!carRepository.existsById(id)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        carToUpdate.setId(id);
+        carRepository.save(carToUpdate);
+        return ResponseEntity.noContent().build();
+    }
+
+
+
+
 }
